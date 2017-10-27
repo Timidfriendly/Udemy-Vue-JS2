96547d5acb7e656811a86e62945aa9b2aff26167
David Knowles <dknowles@xs4all.net>
2017-10-27 16:38:53 +0200

    First dynamic edit

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         title: 'My Vue title'
+    },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        }
     }
 });
