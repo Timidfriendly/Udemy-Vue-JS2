58d2f6c7975a6637fffbbd7edbbea2962284fa7c
David Knowles <dknowles@xs4all.net>
2017-10-30 19:12:08 +0100

    Exercise1 completed

--- exercises/exercise-1/app.js
+++ exercises/exercise-1/app.js
@@ -2,10 +2,16 @@ new Vue({
     el: '#exercise1',
     data: {
         name: 'David Knowles',
-        age: 47
+        age: 47,
+        randomImage: 'https:\/\/upload.wikimedia.org\/wikipedia\/commons\/2\/21\/EverestfromKalarPatarcrop.JPG'
     },
-
-    created: function () {
-        alert('Alert - line number : 8');
+    methods: {
+        randomise: function() {
+            return Math.random();
+        }
     }
+    //,
+    // created: function () {
+    //     alert('Alert - line number : 8');
+    // }
 });
