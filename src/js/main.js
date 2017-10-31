59d76798118931d0df4f7112cdc624f3808cca28
David Knowles <dknowles@xs4all.net>
2017-10-31 08:35:28 +0100

    Section2-Lecture 19. Modifying an Event - with Event Modifiers easier

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,6 @@ new Vue({
         updateCoordinates: function(event) {
             this.x = event.clientX;
             this.y = event.clientY;
-        },
-        donnotListen: function (event) {
-            event.stopPropagation();
         }
     }
 });
