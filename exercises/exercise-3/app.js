e3be7eeab8e1bc8bff98f7a56f26c3958a74fba4
David Knowles <dknowles@xs4all.net>
2017-11-01 18:27:25 +0100

    Exercise 3 - Use computed

--- exercises/exercise-3/app.js
+++ exercises/exercise-3/app.js
@@ -2,5 +2,10 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+            result: function () {
+                return this.value < 37 ? 'not there yet' : 'done';
+            }
         }
     });
