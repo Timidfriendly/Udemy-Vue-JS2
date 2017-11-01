289e78ad5323787bb9c735100e5d7f705a280628
David Knowles <dknowles@xs4all.net>
2017-11-01 19:36:35 +0100

    Exercise 3 - Watch computed value fires only once

--- exercises/exercise-3/app.js
+++ exercises/exercise-3/app.js
@@ -1,20 +1,23 @@
-new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        },
-        computed: {
-            result: function () {
-                return this.value < 37 ? 'not there yet' : 'done';
-            }
-        },
 
-        watch: {
-            result: function (resultChanged) {
+new Vue({
+    el: '#exercise',
+    data: {
+        value: 0,
+        reset_time: 5000
+    },
+    computed: {
+        result: function(){
+            return this.value < 37 ? "Not there yet" : "Done!"
+        }
+    },
+    watch: {
+        result: function(change){
+            if (this.value !== 0) {
                 var vd = this;
-                setTimeout(function () {
+                setTimeout(function() {
                     vd.value = 0;
-                }, 5000);
+                }, vd.reset_time);
             }
         }
-    });
+    }
+});
