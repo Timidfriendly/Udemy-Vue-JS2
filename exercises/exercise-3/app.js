f5460c1accd8b398e7f4bc3dc2b56fffd97e03aa
David Knowles <dknowles@xs4all.net>
2017-11-01 18:51:07 +0100

    Exercise 3 - Watch computed value

--- exercises/exercise-3/app.js
+++ exercises/exercise-3/app.js
@@ -7,5 +7,14 @@ new Vue({
             result: function () {
                 return this.value < 37 ? 'not there yet' : 'done';
             }
+        },
+
+        watch: {
+            result: function (resultChanged) {
+                var vd = this;
+                setTimeout(function () {
+                    vd.value = 0;
+                }, 5000);
+            }
         }
     });
