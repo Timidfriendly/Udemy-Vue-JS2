2cd3a3e89f052e285801d6fcd63c112ab1d51dd8
Timidfriendly <timid@point-online.org>
2017-11-16 21:15:29 +0100

    Section 3 - Lectures 33-43

---
+++ src/js/section-3.js
@@ -0,0 +1,27 @@
+
+new Vue({
+    el: '#Section3_Lecture-33-45',
+    data: {
+        show: true,
+        38: '',
+        ingredients: ['Meat', 'Fruit', 'Cookies'],
+        newIngredient: 'test',
+        persons: [
+            {
+                name: 'Max',
+                age: 27,
+                color: 'Red'
+            },
+            {
+                name: 'Anne',
+                age: 'unknown',
+                color: 'Blues'
+            }
+        ]
+
+    }
+    // ,
+    // created: function () {
+    //     alert('Showing and hiding elements in the dom');
+    // }
+});
